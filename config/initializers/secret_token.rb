d26eda3cbad701b1f5655ac729078c76029bd86b
Joel Mikkelsen <joel@joel-VirtualBox.(none)>
2013-08-02 11:34:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AIM2::Application.config.secret_key_base = 'c9d77b4067d41dce7b64073df8e6814037c654d8d42ed84b0184ab61fef32807cbbec9875ff3bbf3e14ce6fbda3eb582fc6f0b9dcbb97f0d0f4a54d6b64bc4e4'
